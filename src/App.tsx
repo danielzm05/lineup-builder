c6704132b3674199a34f3bf3071c87f85dc8bd07
danielzm05 <danielzmfcbarcelona@gmail.com>
2025-03-26 16:04:22 -0300

    feat: implement lineup selection feature with dynamic formation display

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,49 @@
+import { useState } from "react";
+import { lineups } from "./utils/lineups";
 import "./App.css";
 
+interface FormState {
+  teamName: string;
+  lineupSelected: number;
+}
+
 function App() {
+  const [formValues, setFormValues] = useState<FormState>({
+    teamName: "",
+    lineupSelected: 0,
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormValues((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   return (
     <main>
       <section className="options">
         <form action="">
-          <label htmlFor="formation">
+          <label htmlFor="teamName">
             Team Name:
-            <input type="text" />
+            <input type="text" name="teamName" onChange={handleChange} value={formValues.teamName} maxLength={40} />
           </label>
-          <label htmlFor="formation">
+          <label htmlFor="lineupSelected">
             Formation:
-            <select name="formation" id="">
-              <option value="4-3-3">4-3-3</option>
+            <select name="lineupSelected" onChange={handleChange}>
+              {lineups.map((l, index) => (
+                <option key={l.style} value={index}>
+                  {l.formation}
+                </option>
+              ))}
             </select>
           </label>
         </form>
       </section>
       <section className="field-container ">
         <header>
-          <p>Team Name</p>
-          <p>4-3-3</p>
+          <p>{formValues.teamName}</p>
+          <p>{lineups[formValues.lineupSelected].formation}</p>
         </header>
-        <div className="field ">
+        <div className="field" style={{ gridTemplateAreas: lineups[formValues.lineupSelected].style }}>
           <div className="player"></div>
           <div className="player"></div>
           <div className="player"></div>
---
+++ src/utils/lineups.tsx
@@ -0,0 +1,89 @@
+interface Lineup {
+  formation: string;
+  style: string;
+}
+
+export const lineups: Lineup[] = [
+  {
+    formation: "4-3-3",
+    style: `". area11 . area9 . area7 ."
+      ". area8 . area5 . area10 ."
+      "area3 . area6 . area2 . area4"
+      ". . . area1 . . ."`,
+  },
+  {
+    formation: "4-4-2",
+    style: `". . area9 . area7 . ."
+      "area11 . area8 . area5 . area10"
+      "area3 . area6 . area2 . area4"
+      ". . . area1 . . ."`,
+  },
+  {
+    formation: "4-2-3-1",
+    style: `". . area9 .  ."
+      "area11 . area10 . area7"
+      ". area8 . area5  ."
+      "area3 area6 . area2 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "3-5-2",
+    style: `". area7 . area9 ."
+      "area11 area8 . area10 area4"
+      ". . area5 . ."
+      "area3 . area6 . area2"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "5-3-2",
+    style: `". area9 . area7 ."
+      "area11 . area8 . area10"
+      "area3 area5 area6  area2 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "4-1-4-1",
+    style: `". . area9 . ."
+      "area11 area8 area10 area7"
+      ". . area5 . ."
+      "area3 area2 . area6 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "3-4-3",
+    style: `"area11 . area9 . area7"
+      "area3 area8 . area10 area4"
+      ". area6 area5 area2 ."
+      ". . area1 . ."`,
+  },
+  {
+    formation: "4-5-1",
+    style: `". . area9 . ."
+      "area11 area8 area5 area10 area7"
+      "area3 area6 . area2 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "4-3-1-2",
+    style: `". area9 . area7 ."
+      ". . area8 . ."
+      "area11 . area5 . area10"
+      "area3 area6 . area2 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "4-3-2-1",
+    style: `". . area9 . ."
+      ". area8 . area10 ."
+      "area11 . area5 . area7"
+      "area3 area6 . area2 area4"
+      ". . area1 . ."`,
+  },
+  {
+    formation: "5-4-1",
+    style: `". . area9 . ."
+      "area11 area8 . area10 area7"
+      "area3 area5 area6  area2 area4"
+      ". . area1 . ."`,
+  },
+];
